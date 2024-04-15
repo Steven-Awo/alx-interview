6b77713c426c17956304541d9625b41f4008b329
StevenAwo <rotimiawomodu@gmail.com>
2024-04-15 12:52:12 +0100

    TASK 0

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""the method that's to determine if all the boxes could
+actually be opened"""
+
+
+def canUnlockAll(boxes):
+
+    if (len(boxes) == 0):
+        return False
+    
+    if (type(boxes) is not list):
+        return False
+
+    keyys = [0]
+    for k in keyys:
+        for b in boxes[k]:
+            if b not in keyys and b != k and b < len(boxes) and b != 0:
+                keyys.append(b)
+    if len(keyys) == len(boxes):
+        return True
+    else:
+        return False
